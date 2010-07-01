fba502a1b58e42b073f89b0e0af686cf7740442b
jbandi <dev@jonasbandi.net>
2010-07-01 10:25:01 +0200

    Adding feature-example for StepArgumentTransformation and how to use custom culture for numbers and dates.

---
+++ features/SpecFlow.Examples.StepArgumentTransformation/ConvertSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.Example.StepArgumentTransformation
+{
+    [Binding]
+    public class AustrianLocalizationTransformer
+    {
+        [StepArgumentTransformation]
+        public double TransformDouble(string expr)
+        {
+            return Convert.ToDouble(expr, CultureInfo.GetCultureInfo("de-AT"));
+        }      
+        
+        [StepArgumentTransformation]
+        public DateTime TransformDate(string expr)
+        {
+            return Convert.ToDateTime(expr, CultureInfo.GetCultureInfo("de-AT"));
+        }
+    }
+    [Binding]
+    public class ConvertSteps
+    {
+        [Given("I have entered (.*) into the system")]
+        public void GivenIHaveEnteredSomethingIntoTheSystem(double number)
+        {
+            Assert.That(number, Is.EqualTo(1050.1));
+        }
+
+        [Given("the date is (.*)")]
+        public void TheDateIs(DateTime dateTime)
+        {
+            Assert.That(dateTime, Is.EqualTo(new DateTime(2010,12,22)));
+        }
+    }
+}
---
+++ features/SpecFlow.Examples.StepArgumentTransformation/ConvertTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace SpecFlow.Example.Localization
+{
+    [TestFixture]
+    public class ConverterTest
+    {
+        [Test]
+        public void ConvertStringToDouble_Exploration()
+        {
+            Assert.AreEqual(1050.1, Convert.ToDouble("1,050.1", CultureInfo.GetCultureInfo("en-US")));
+            Assert.AreEqual(1050.1, Convert.ToDouble("1.050,1", CultureInfo.GetCultureInfo("de-AT")));
+            Assert.AreEqual(1050.1, Convert.ToDouble("1'050.1", CultureInfo.GetCultureInfo("de-CH")));
+        }
+    }
+}
