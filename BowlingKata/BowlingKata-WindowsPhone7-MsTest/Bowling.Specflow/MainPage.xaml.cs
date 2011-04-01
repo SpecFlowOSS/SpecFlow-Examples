493029a945d1d4a6993816551b84317044687787
aximus <aximus@tut.by>
2011-04-02 01:05:43 +0300

    added WP7 version of BowlingKata

---
+++ BowlingKata/BowlingKata-WindowsPhone7-MsTest/Bowling.Specflow/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+using Microsoft.Silverlight.Testing;
+
+namespace Bowling.Specflow
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			var testPage = UnitTestSystem.CreateTestPage() as IMobileTestPage;
+			BackKeyPress += (x, xe) => xe.Cancel = testPage.NavigateBack();
+			(Application.Current.RootVisual as PhoneApplicationFrame).Content = testPage;
+		}
+	}
+}
