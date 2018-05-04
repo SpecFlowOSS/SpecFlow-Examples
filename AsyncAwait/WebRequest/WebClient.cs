5e4c84ec0da6ae987850527e3de2acefeaadd792
David Eiwen <david-e-winlive@live.at>
2018-05-04 12:10:12 +0200

    Added example for using async-await in SpecFlow

---
+++ AsyncAwait/WebRequest/WebClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebRequest
+{
+    public class WebClient
+    {
+        public async Task<HttpResponseMessage> Get(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return await httpClient.GetAsync(url);
+            }
+        }
+    }
+}
