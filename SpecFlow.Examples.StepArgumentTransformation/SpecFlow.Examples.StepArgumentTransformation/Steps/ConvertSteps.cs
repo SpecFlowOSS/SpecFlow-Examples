0e495c772b96290ff235184a9f023337a794e2a6
David Eiwen <david-e-winlive@live.at>
2018-08-21 13:19:24 +0200

    Updated Examples to SpecFlow Version 2.4 (#24)
    * Updated BowlingKata-XUnit example
    
    * Updated BowlingKata-VB-MsTest example
    
    * Updated BowlingKata-NUnit example
    
    * Updated BowlingKata-MsTest example
    
    * Updated BowlingKata-German example
    
    * Updated BowlingKata-GenerateTestsFromMsBuild example
    
    * Updated BowlingKata-Fsharp example
    
    * Updated BowlingKata-FluentAssertions example
    
    * Updated StepArgumentTransformation example
    
    * Updated SpecialChars example
    
    * Updated AsyncAwait example
    
    * Updated packages of ASP.NET BookShop example

---
+++ SpecFlow.Examples.StepArgumentTransformation/SpecFlow.Examples.StepArgumentTransformation/Steps/ConvertSteps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.Example.StepArgumentTransformation.Steps
+{
+    [Binding]
+    public class ConvertSteps
+    {
+        [Given("I have entered (.*) into the system")]
+        public void GivenIHaveEnteredSomethingIntoTheSystem(double number)
+        {
+            number.Should().Be(1050.1);
+        }
+
+        [Given("the date is (.*)")]
+        public void TheDateIs(DateTime dateTime)
+        {
+            dateTime.Should().Be(new DateTime(2010, 12, 22));
+        }
+    }
+}
