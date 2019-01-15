2abb063c860b735abbff3019d91bd35019555c10
Sophie Keiblinger <sk@techtalk.at>
2019-01-15 20:17:36 +0100

    Add Gherkin Formatting Examples Solution

---
+++ GherkinFormattingExamples/GherkinFormattingExamples/GherkinFormattingExamples/Features/GherkinFormattingExamples.feature
@@ -0,0 +1,74 @@
+﻿Feature: GherkinFormattingExamples
+
+#-------------------------------------------------------------
+Scenario: Discernible Given-When-Then Blocks
+	In order to quickly spot where one block ends and another one begins, 
+	you can indent the steps starting with “And”
+
+	Given I need to prepare some data for my scenario
+		
+		And this is more complex so I need a second step
+
+		And this is more complex so I need a third step
+
+	When I trigger some action 
+
+	Then I can see the expected outcome 
+		
+		And this outcome also has a second step
+
+		And this outcome also has a third step
+
+
+#-------------------------------------------------------------
+Scenario: Discernible Given-When-Then Blocks with Tables
+	Using tables this convention might make it uneasy for the eye
+
+	Given I need to prepare the following data for my scenario:
+		| column 1  | column 2 |
+		| necessary | data     |
+		
+		And this is more complex so I need a second step with a table:
+			| column x | column y |
+			| more     | data     |
+
+		And this is more complex so I need a third step with a table:
+			| column z | column c |
+			| more     | data     |
+
+	When I trigger some action 
+
+	Then I can see the expected outcome 
+		
+		And this outcome also has a second step
+
+		And this outcome also has a third step
+
+
+#-------------------------------------------------------------
+Scenario: Discernible Given-When-Then Blocks with Tables - Alternative
+	be aware that newlines tend to elongate your scenario 
+
+	Given I need to prepare the following data for my scenario:
+		| column 1  | column 2 |
+		| necessary | data     |
+		
+	And this is more complex so I need a second step with a table:
+		| column x | column y |
+		| more     | data     |
+
+	And this is more complex so I need a third step with a table:
+		| column z | column c |
+		| more     | data     |
+
+
+	When I trigger some action 
+
+
+	Then I can see the expected outcome 
+		
+	And this outcome also has a second step
+
+	And this outcome also has a third step
+
+
---
+++ GherkinFormattingExamples/GherkinFormattingExamples/GherkinFormattingExamples/StepDefinitions/GherkinFormattingExamplesSteps.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GherkinFormattingExamples.StepDefinitions
+{
+    [Binding]
+    public class GherkinFormattingExamplesSteps
+    {
+        [Given(@"I need to prepare some data for my scenario")]
+        public void GivenINeedToPrepareSomeDataForMyScenario()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"this is more complex so I need a second step")]
+        public void GivenThisIsMoreComplexSoINeedASecondStep()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"this is more complex so I need a third step")]
+        public void GivenThisIsMoreComplexSoINeedAThirdStep()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I trigger some action")]
+        public void WhenITriggerTheAction()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I can see the expected outcome")]
+        public void ThenICanSeeTheExpectedOutcome()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"this outcome also has a second step")]
+        public void ThenThisOutcomeAlsoHasASecondStep()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"this outcome also has a third step")]
+        public void ThenThisOutcomeAlsoHasAThirdStep()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I need to prepare the following data for my scenario:")]
+        public void GivenINeedToPrepareTheFollowingDataForMyScenario(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"this is more complex so I need a second step with a table:")]
+        public void GivenThisIsMoreComplexSoINeedASecondStepWithATable(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"this is more complex so I need a third step with a table:")]
+        public void GivenThisIsMoreComplexSoINeedAThirdStepWithATable(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
