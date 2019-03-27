fbfa2497678f5e6ef999e2508924cac2a8038653
David Eiwen <david-e-winlive@live.at>
2019-03-27 14:43:45 +0100

    update specflow to current preview

--- ASP.NET-MVC/BookShop/BookShop.AcceptanceTests/runtests.cmd
+++ ASP.NET-MVC/BookShop/BookShop.AcceptanceTests/runtests.cmd
@@ -11,12 +11,12 @@
 
 @if ERRORLEVEL 1 goto end
 
-@cd ..\packages\SpecRun.Runner.*\tools
+@cd ..\packages\SpecRun.Runner.*\tools\net45
 
 @set profile=%1
 @if "%profile%" == "" set profile=Default
 
-SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\Debug" /log:specrun.log %2 %3 %4 %5
+SpecRun.exe run "%profile%.srprofile" --baseFolder "%~dp0\bin\Debug" --log "specrun.log" %2 %3 %4 %5
 
 :end
 
