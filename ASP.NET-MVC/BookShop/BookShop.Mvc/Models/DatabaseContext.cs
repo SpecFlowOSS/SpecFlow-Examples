2dcd7e8abad3fc93e1263a20261ab895b67320f7
Manuel Eiwen <manuel.eiwen@gmx.at>
2019-08-07 13:20:55 +0200

    Remove InMemoryDbContext

--- ASP.NET-MVC/BookShop/BookShop.Mvc/Models/DatabaseContext.cs
+++ ASP.NET-MVC/BookShop/BookShop.Mvc/Models/DatabaseContext.cs
@@ -41,27 +41,4 @@ namespace BookShop.Mvc.Models
 
         public DbSet<OrderLine> OrderLines { get; set; }
     }
-
-    public class InMemoryDbContext
-        : DbContext, IDatabaseContext
-    {
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseInMemoryDatabase("BookShop");
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<OrderLine>()
-                .HasKey(ol => new { ol.BookId, ol.OrderId });
-        }
-
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<Book> Books { get; set; }
-        public DbSet<OrderLine> OrderLines { get; set; }
-    }
 }
