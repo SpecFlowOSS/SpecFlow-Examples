21a1998f2e360d374258fca3f74342621d056632
Manuel Eiwen <manuel.eiwen@gmx.at>
2019-08-08 10:40:49 +0200

    Make search case-insensitive

--- ASP.NET-MVC/BookShop/BookShop.Mvc/Controllers/CatalogController.cs
+++ ASP.NET-MVC/BookShop/BookShop.Mvc/Controllers/CatalogController.cs
@@ -19,8 +19,8 @@ namespace BookShop.Mvc.Controllers
             var terms = searchTerm?.Split(' ') ?? new string[0];
             var predicate = terms.Aggregate(
                 PredicateBuilder.New<Book>(string.IsNullOrEmpty(searchTerm)),
-                (acc, term) => acc.Or(b => b.Title.Contains(term))
-                                  .Or(b => b.Author.Contains(term)));
+                (acc, term) => acc.Or(b => b.Title.ToLower().Contains(term.ToLower()))
+                                  .Or(b => b.Author.ToLower().Contains(term.ToLower())));
 
             var books = _databaseContext.Books.AsExpandable()
                 .Where(predicate)
