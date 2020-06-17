58fb4a0b7c7b4add13abff0d93874e576ce1656e
Andreas Willich <andreas@willich.io>
2020-06-17 16:59:35 +0200

    last changes

--- ASP.NET-MVC/BookShop/BookShop.Mvc/Startup.cs
+++ ASP.NET-MVC/BookShop/BookShop.Mvc/Startup.cs
@@ -54,7 +54,7 @@ namespace BookShop.Mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
