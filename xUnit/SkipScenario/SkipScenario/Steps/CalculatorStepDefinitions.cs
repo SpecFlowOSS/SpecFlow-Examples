36ca205f5d2d9f73e9319fed79279a7072114fe8
Andreas Willich <andreas@willich.io>
2020-07-17 10:11:40 +0200

    add xUnit skip scenario example

---
+++ xUnit/SkipScenario/SkipScenario/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,37 @@
+﻿using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SkipScenario.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
+        {
+  
+        }
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {
+            Skip.If(true, "This scenario is always skipped");
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int result)
+        {
+
+        }
+    }
+}
