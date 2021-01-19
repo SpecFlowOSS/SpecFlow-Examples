2a89c33582e78551b9a63408f5fd52fc7491cccf
Zsolt Olah <z.olah@tricentis.com>
2021-01-19 19:47:57 +0100

    Add Decorator example

---
+++ Decorators/SampleMethodTagDecorator/GeneratorPlugin/SampleGeneratorPlugin.cs
@@ -0,0 +1,25 @@
+﻿using SampleGeneratorPlugin;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestConverter;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly:GeneratorPlugin(typeof(GeneratorPlugin.SampleGeneratorPlugin))]
+
+namespace GeneratorPlugin
+{
+    public class SampleGeneratorPlugin : IGeneratorPlugin
+    {
+	    public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters,
+		    UnitTestProviderConfiguration unitTestProviderConfiguration)
+	    {
+		    // Register the decorator
+		    generatorPluginEvents.RegisterDependencies += RegisterDependencies;
+	    }
+
+	    private void RegisterDependencies(object sender, RegisterDependenciesEventArgs eventArgs)
+	    {
+		    eventArgs.ObjectContainer.RegisterTypeAs<MyMethodTagDecorator, ITestMethodTagDecorator>(MyMethodTagDecorator.TAG_NAME);
+	    }
+    }
+}
