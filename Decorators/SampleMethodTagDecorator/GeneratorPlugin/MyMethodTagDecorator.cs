bb46ecde6b91fce4a57b7c5bc70d7fcf475cd3d9
Zsolt Olah <56628780+ozslab@users.noreply.github.com>
2021-01-20 09:55:57 +0100

    Add Decorator example (#60)

---
+++ Decorators/SampleMethodTagDecorator/GeneratorPlugin/MyMethodTagDecorator.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom;
+using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.UnitTestConverter;
+
+namespace SampleGeneratorPlugin
+{
+	public class MyMethodTagDecorator : ITestMethodTagDecorator
+	{
+
+		public static readonly string TAG_NAME = "myMethodTagDecorator";
+		private readonly ITagFilterMatcher _tagFilterMatcher;
+
+		public MyMethodTagDecorator(ITagFilterMatcher tagFilterMatcher)
+		{
+			_tagFilterMatcher = tagFilterMatcher;
+		}
+
+		public bool CanDecorateFrom(string tagName, TestClassGenerationContext generationContext, CodeMemberMethod testMethod)
+		{
+			return _tagFilterMatcher.Match(TAG_NAME, tagName);
+		}
+
+		public void DecorateFrom(string tagName, TestClassGenerationContext generationContext, CodeMemberMethod testMethod)
+		{
+			var attribute = new CodeAttributeDeclaration(
+				"NUnit.Framework.ApartmentAttribute",
+				new CodeAttributeArgument(
+					new CodeFieldReferenceExpression(
+						new CodeTypeReferenceExpression(typeof(System.Threading.ApartmentState)),
+						"STA")));
+
+			testMethod.CustomAttributes.Add(attribute);
+		}
+
+		public int Priority { get; }
+		public bool RemoveProcessedTags { get; }
+		public bool ApplyOtherDecoratorsForProcessedTags { get; }
+	}
+}
