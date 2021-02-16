4cd4a549263f1f33f9eec45f6a2b0ca334e6bc12
Balázs Epresi <epresib@gmail.com>
2021-02-16 14:27:36 +0100

    Update NuGet package versions to * (highest stable version) (#65)
    * Create a tool for building the solutions and run the tests
    
    * Adjust pipeline to run the tool
    
    * Build Decorator plugin first
    
    * Temporarily fix ExternalDataSample
    
    * Update NuGet package versions to * (highest stable version)
    
    https://docs.microsoft.com/en-us/nuget/concepts/package-versioning#floating-version-resolutions

---
+++ buildandtest.ps1
@@ -0,0 +1,43 @@
+$skipTestExecution = ('*ExternalDataSample\Specs.sln', '*GherkinFormattingExamples\GherkinFormattingExamples.sln')
+
+ForEach ($file in get-childitem . -recurse | where {$_.extension -like "*sln"})
+{
+	$fullname = $file.fullname
+	
+	# MSBuild contains old specflow versions, do not touch it
+	if (!($fullname -match 'MSBuild'))
+	{		
+		Write-Output "File name: $file"
+		Write-Output "Fullpath: $fullname"	
+		
+		# dotnet test restores and builds the solution
+		if (!(($skipTestExecution | %{$fullname -like $_}) -contains $true))
+		{
+			# it has a local nuget feed, so build the nuget package first
+			if ($fullname -match 'SampleMethodTagDecorator')
+			{
+				$generatorPlugin = $fullname.Replace('SampleMethodTagDecorator.sln', 'GeneratorPlugin\SampleGeneratorPlugin.csproj')
+				Write-Output $generatorPlugin
+				iex "dotnet build '$generatorPlugin'"
+			}
+			
+			iex "dotnet test '$fullname'"
+			if ($lastexitcode -ne 0)
+			{
+				break;
+			}	
+		}
+		else 
+		{
+			Write-Output "Test execution of '$fullname' was skipped because it contains failing tests. Building solution."
+			
+			iex "dotnet build '$fullname'"
+			if ($lastexitcode -ne 0)
+			{
+				break;
+			}
+		}
+
+		Write-Output "------------------------------------------------------------------------------------------------------------"		
+	}
+}
