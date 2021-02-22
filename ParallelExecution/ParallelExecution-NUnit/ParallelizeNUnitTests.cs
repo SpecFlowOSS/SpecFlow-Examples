9f2d20854d789a6170ff8f7b8dbfae493ace284e
Zoltan Toth <z.toth@tricentis.com>
2021-02-22 11:59:46 +0100

    Fix phrasing

--- ParallelExecution/ParallelExecution-NUnit/ParallelizeNUnitTests.cs
+++ ParallelExecution/ParallelExecution-NUnit/ParallelizeNUnitTests.cs
@@ -3,10 +3,11 @@
 // By default NUnit does not run tests in parallel
 // https://docs.nunit.org/articles/nunit/writing-tests/attributes/parallelizable.html
 
-// You can configure NUnit on assembly level to let all SpecFlow features run in parallel with each other
+// You can configure on assembly level to run SpecFlow features in parallel with each other
 [assembly: Parallelizable(ParallelScope.Fixtures)]
 
 
-// Note: SpecFlow does not support scenario level parallelization with NUnit (when scenarios from the same feature execute in parallel).
+// Note: SpecFlow does not support scenario level parallelization (when scenarios from the same feature execute in parallel).
+// https://github.com/SpecFlowOSS/SpecFlow/issues/1535
 // If you configure a higher level NUnit parallelization than "Fixtures" your tests will fail with runtime errors.
 // E.g. [assembly: Parallelizable(ParallelScope.Children)] will not work.
