6c2fd2471ed2d237d3fa8c548122d8f3913bd474
Zoltan Toth <z.toth@tricentis.com>
2021-03-31 11:44:11 +0200

    Cleanup add button

--- CalculatorSelenium/CalculatorSelenium.Specs/PageObjects/CalculatorPageObject.cs
+++ CalculatorSelenium/CalculatorSelenium.Specs/PageObjects/CalculatorPageObject.cs
@@ -18,7 +18,7 @@ namespace CalculatorSelenium.Specs.PageObjects
         //Find elements by ID
         private IWebElement FirstNumberElement => _webDriver.FindElement(By.Id("first-number"));
         private IWebElement SecondNumberElement => _webDriver.FindElement(By.Id("second-number"));
-        private IWebElement AddButtonElement => _webDriver.FindElement(By.Id("add-slow-button"));
+        private IWebElement AddButtonElement => _webDriver.FindElement(By.Id("add-button"));
         private IWebElement ResultElement => _webDriver.FindElement(By.Id("result"));
 
         public void EnterFirstNumber(string number)
