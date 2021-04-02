71827a0ec0742f3fd850df430673467748d3ffa8
Ali Mollahosseini <73827414+alimogithub@users.noreply.github.com>
2021-04-02 15:24:14 +0200

    Calculator selenium (#67)
    * First draft with Runner and Thread.Sleep
    
    * Wait for non empty result, helper methods
    
    * Cleanup
    
    * Simplify, comments
    
    * Cleanup add button
    
    * Change to NUnit
    
    * Single browser, reset in hook
    
    * Add comments
    
    * Add comments
    
    * packages
    
    * typo
    
    Co-authored-by: Zoltan Toth <z.toth@tricentis.com>

---
+++ CalculatorSelenium/CalculatorSelenium.Specs/Drivers/BrowserDriver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CalculatorSelenium.Specs.Drivers
+{
+    /// <summary>
+    /// Manages a browser instance using Selenium
+    /// </summary>
+    public class BrowserDriver : IDisposable
+    {
+        private readonly Lazy<IWebDriver> _currentWebDriverLazy;
+        private bool _isDisposed;
+
+        public BrowserDriver()
+        {
+            _currentWebDriverLazy = new Lazy<IWebDriver>(CreateWebDriver);
+        }
+
+        /// <summary>
+        /// The Selenium IWebDriver instance
+        /// </summary>
+        public IWebDriver Current => _currentWebDriverLazy.Value;
+
+        /// <summary>
+        /// Creates the Selenium web driver (opens a browser)
+        /// </summary>
+        /// <returns></returns>
+        private IWebDriver CreateWebDriver()
+        {
+            //We use the Chrome browser
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+
+            var chromeOptions = new ChromeOptions();
+
+            var chromeDriver = new ChromeDriver(chromeDriverService, chromeOptions);
+
+            return chromeDriver;
+        }
+
+        /// <summary>
+        /// Disposes the Selenium web driver (closing the browser)
+        /// </summary>
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (_currentWebDriverLazy.IsValueCreated)
+            {
+                Current.Quit();
+            }
+
+            _isDisposed = true;
+        }
+    }
+}
---
+++ CalculatorSelenium/CalculatorSelenium.Specs/PageObjects/CalculatorPageObject.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace CalculatorSelenium.Specs.PageObjects
+{
+    /// <summary>
+    /// Calculator Page Object
+    /// </summary>
+    public class CalculatorPageObject
+    {
+        //The URL of the calculator to be opened in the browser
+        private const string CalculatorUrl = "https://specflowoss.github.io/Calculator-Demo/Calculator.html";
+
+        //The Selenium web driver to automate the browser
+        private readonly IWebDriver _webDriver;
+        
+        //The default wait time in seconds for wait.Until
+        public const int DefaultWaitInSeconds = 5;
+
+        public CalculatorPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        //Finding elements by ID
+        private IWebElement FirstNumberElement => _webDriver.FindElement(By.Id("first-number"));
+        private IWebElement SecondNumberElement => _webDriver.FindElement(By.Id("second-number"));
+        private IWebElement AddButtonElement => _webDriver.FindElement(By.Id("add-button"));
+        private IWebElement ResultElement => _webDriver.FindElement(By.Id("result"));
+        private IWebElement ResetButtonElement => _webDriver.FindElement(By.Id("reset-button"));
+
+        public void EnterFirstNumber(string number)
+        {
+            //Clear text box
+            FirstNumberElement.Clear();
+            //Enter text
+            FirstNumberElement.SendKeys(number);
+        }
+
+        public void EnterSecondNumber(string number)
+        {
+            //Clear text box
+            SecondNumberElement.Clear();
+            //Enter text
+            SecondNumberElement.SendKeys(number);
+        }
+
+        public void ClickAdd()
+        {
+            //Click the add button
+            AddButtonElement.Click();
+        }
+
+        public void EnsureCalculatorIsOpenAndReset()
+        {
+            //Open the calculator page in the browser if not opened yet
+            if (_webDriver.Url != CalculatorUrl)
+            {
+                _webDriver.Url = CalculatorUrl;
+            }
+            //Otherwise reset the calculator by clicking the reset button
+            else
+            {
+                //Click the rest button
+                ResetButtonElement.Click();
+
+                //Wait until the result is empty again
+                WaitForEmptyResult();
+            }
+        }
+
+        public string WaitForNonEmptyResult()
+        {
+            //Wait for the result to be not empty
+            return WaitUntil(
+                () => ResultElement.GetAttribute("value"),
+                result => !string.IsNullOrEmpty(result));
+        }
+
+        public string WaitForEmptyResult()
+        {
+            //Wait for the result to be empty
+            return WaitUntil(
+                () => ResultElement.GetAttribute("value"),
+                result => result == string.Empty);
+        }
+
+        /// <summary>
+        /// Helper method to wait until the expected result is available on the UI
+        /// </summary>
+        /// <typeparam name="T">The type of result to retrieve</typeparam>
+        /// <param name="getResult">The function to poll the result from the UI</param>
+        /// <param name="isResultAccepted">The function to decide if the polled result is accepted</param>
+        /// <returns>An accepted result returned from the UI. If the UI does not return an accepted result within the timeout an exception is thrown.</returns>
+        private T WaitUntil<T>(Func<T> getResult, Func<T, bool> isResultAccepted) where T: class
+        {
+            var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(DefaultWaitInSeconds));
+            return wait.Until(driver =>
+            {
+                var result = getResult();
+                if (!isResultAccepted(result))
+                    return default;
+
+                return result;
+            });
+
+        }
+    }
+}
