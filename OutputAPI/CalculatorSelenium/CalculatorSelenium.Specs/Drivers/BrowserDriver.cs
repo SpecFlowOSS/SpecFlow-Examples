8f17ec1467a2ff15a5fb2bdbd1ab3250bdfdfcf2
Andreas Willich <a.willich@tricentis.com>
2021-05-14 14:26:55 +0200

    Add example for the Output API

---
+++ OutputAPI/CalculatorSelenium/CalculatorSelenium.Specs/Drivers/BrowserDriver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace CalculatorSelenium.Specs.Drivers
+{
+    /// <summary>
+    /// Manages a browser instance using Selenium
+    /// </summary>
+    public class BrowserDriver : IDisposable
+    {
+        private readonly ISpecFlowOutputHelper _specFlowOutputHelper;
+        private readonly Lazy<IWebDriver> _currentWebDriverLazy;
+        private bool _isDisposed;
+
+        public BrowserDriver(ISpecFlowOutputHelper specFlowOutputHelper)
+        {
+            _specFlowOutputHelper = specFlowOutputHelper;
+            _currentWebDriverLazy = new Lazy<IWebDriver>(CreateWebDriver);
+        }
+
+        /// <summary>
+        /// The Selenium IWebDriver instance
+        /// </summary>
+        public IWebDriver Current => _currentWebDriverLazy.Value;
+
+        /// <summary>
+        /// Creates the Selenium web driver (opens a browser)
+        /// </summary>
+        /// <returns></returns>
+        private IWebDriver CreateWebDriver()
+        {
+            //We use the Chrome browser
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+
+            var chromeOptions = new ChromeOptions();
+
+            var chromeDriver = new ChromeDriver(chromeDriverService, chromeOptions);
+
+            _specFlowOutputHelper.WriteLine("Browser launched");
+
+            return chromeDriver;
+        }
+
+        /// <summary>
+        /// Disposes the Selenium web driver (closing the browser)
+        /// </summary>
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (_currentWebDriverLazy.IsValueCreated)
+            {
+                Current.Quit();
+
+                _specFlowOutputHelper.WriteLine("Browser closed");
+            }
+
+            _isDisposed = true;
+        }
+    }
+}
---
+++ OutputAPI/CalculatorSelenium/CalculatorSelenium.Specs/Hooks/LoggingHooks.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using CalculatorSelenium.Specs.Drivers;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace CalculatorSelenium.Specs.Hooks
+{
+    [Binding]
+    public class LoggingHooks
+    {
+        private readonly BrowserDriver _browserDriver;
+        private readonly ISpecFlowOutputHelper _specFlowOutputHelper;
+
+        public LoggingHooks(BrowserDriver browserDriver, ISpecFlowOutputHelper specFlowOutputHelper)
+        {
+            _browserDriver = browserDriver;
+            _specFlowOutputHelper = specFlowOutputHelper;
+        }
+
+        [AfterStep()]
+        public void TakeScreenshotAfterEachStep()
+        {
+
+            if (_browserDriver.Current is ITakesScreenshot screenshotTaker)
+            {
+                var filename = Path.ChangeExtension(Path.GetRandomFileName(), "png");
+
+                screenshotTaker.GetScreenshot().SaveAsFile(filename);
+
+                _specFlowOutputHelper.AddAttachment(filename);
+            }
+        }
+    }
+}
