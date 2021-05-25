9e41e853144c8d0ee47325ca8f91da69bc3e8380
Andreas Willich <a.willich@tricentis.com>
2021-05-25 16:36:53 +0200

    revert output change

--- Webinars/2021-05-26 Output API/CommunityContentSubmissionPage/CommunityContentSubmissionPage.Specs/Hooks/LoggingHooks.cs
+++ Webinars/2021-05-26 Output API/CommunityContentSubmissionPage/CommunityContentSubmissionPage.Specs/Hooks/LoggingHooks.cs
@@ -26,7 +26,7 @@ namespace CommunityContentSubmissionPage.Specs.Hooks
 
             _specFlowOutputHelper.WriteLine($"Current URL: {_actor.AsksFor(CurrentUrl.FromBrowser())}"); 
 
-            _specFlowOutputHelper.AddAttachment($"bin/Debug/net5.0/{screenshotLocation}");
+            _specFlowOutputHelper.AddAttachment(screenshotLocation));
         }
 
 
