06801182540752b63261b64bc070e0a7f4474ede
Viktor Nemes <nemesv@gmail.com>
2021-06-29 08:21:47 +0200

    Add comments

--- CalculatorSelenium/CalculatorSelenium.Specs/Drivers/BlobLogger.cs
+++ CalculatorSelenium/CalculatorSelenium.Specs/Drivers/BlobLogger.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Reflection.PortableExecutable;
-using System.Text;
 using System.Threading.Tasks;
-using Azure;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
-using TechTalk.SpecFlow;
 
 namespace CalculatorSelenium.Specs.Drivers
 {
@@ -33,11 +27,13 @@ namespace CalculatorSelenium.Specs.Drivers
             string containerName =
                 Assembly.GetExecutingAssembly().GetName().Name + DateTime.Now.ToString("s");
 
+            //the container name can only contain lowercase letters, numbers and dashes (-)
             containerName = containerName
                 .Replace(":", "-")
                 .Replace(".", "-")
                 .ToLower();
 
+            //we need to set the PublicAccessType.Blob so any user can open the images from the container
             var response = await _blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
             _containerClient = response.Value;
         }
@@ -51,6 +47,7 @@ namespace CalculatorSelenium.Specs.Drivers
                 Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ".png");
 
             using MemoryStream uploadFileStream = new MemoryStream(imageData);
+            //we need to set the correct header to view the file in the browser
             var headers = new BlobHttpHeaders() { ContentType = "image/png" };
             await blobClient.UploadAsync(uploadFileStream, headers);
 
