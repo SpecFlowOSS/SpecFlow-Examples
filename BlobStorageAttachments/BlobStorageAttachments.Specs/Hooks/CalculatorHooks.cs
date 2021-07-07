e8cf71718646e3bec0719398edf420d0cd709557
Viktor Nemes <nemesv@gmail.com>
2021-07-07 09:13:49 +0200

    Add a sample project to showcase how to use the SpecFlow output API with an external storage like Azure Blob Storage

---
+++ BlobStorageAttachments/BlobStorageAttachments.Specs/Hooks/CalculatorHooks.cs
@@ -0,0 +1,23 @@
+﻿using BlobStorageAttachments.Specs.Drivers;
+using BlobStorageAttachments.Specs.PageObjects;
+using TechTalk.SpecFlow;
+
+namespace BlobStorageAttachments.Specs.Hooks
+{
+    /// <summary>
+    /// Calculator related hooks
+    /// </summary>
+    [Binding]
+    public class CalculatorHooks
+    {
+        ///<summary>
+        ///  Reset the calculator before each scenario tagged with "Calculator"
+        /// </summary>
+        [BeforeScenario("Calculator")]
+        public static void BeforeScenario(BrowserDriver browserDriver)
+        {
+            var calculatorPageObject = new CalculatorPageObject(browserDriver.Current);
+            calculatorPageObject.EnsureCalculatorIsOpenAndReset();
+        }
+    }
+}
---
+++ BlobStorageAttachments/BlobStorageAttachments.Specs/Hooks/LoggingHoks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BlobStorageAttachments.Specs.Drivers;
+using OpenQA.Selenium.Support.Extensions;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace BlobStorageAttachments.Specs.Hooks
+{
+    [Binding]
+    public sealed class LoggingHooks
+    {
+        private readonly ISpecFlowOutputHelper _specFlowOutputHelper;
+        private readonly BrowserDriver _browserDriver;
+        private string? _attachmentTemplate;
+
+        public LoggingHooks(ISpecFlowOutputHelper specFlowOutputHelper, BrowserDriver browserDriver)
+        {
+            _specFlowOutputHelper = specFlowOutputHelper;
+            _browserDriver = browserDriver;
+            _attachmentTemplate = Environment.GetEnvironmentVariable("ATTACHMENT_TEMPLATE");
+        }
+
+        [AfterStep]
+        public void AfterStep()
+        {
+            var screenshot = _browserDriver.Current.TakeScreenshot();
+            var imagePath = Path.GetFileNameWithoutExtension(Path.GetTempFileName()) + ".png";
+            Directory.CreateDirectory("Screenshots");
+            screenshot.SaveAsFile(Path.Combine("Screenshots", imagePath));
+            if (!string.IsNullOrEmpty(_attachmentTemplate))
+                _specFlowOutputHelper.AddAttachment(string.Format(_attachmentTemplate, imagePath));
+        }
+    }
+}
