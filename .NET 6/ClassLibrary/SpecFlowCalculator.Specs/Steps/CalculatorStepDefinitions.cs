55bcff8f5d50daa2f312f3d61e076210f42d9b2c
Jordan Western <j.western@tricentis.com>
2021-09-10 15:51:49 +0100

    project set up complete

---
+++ .NET 6/ClassLibrary/SpecFlowCalculator.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowCalculator.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly Calculator _calculator;
+        private int _result;
+
+        public CalculatorStepDefinitions(Calculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Add();
+        }
+
+        [When("the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _calculator.Subtract();
+        }
+
+        [When("the two numbers are divided")]
+        public void WhenTheTwoNumbersAreDivided()
+        {
+            _result = _calculator.Divide();
+        }
+
+        [When("the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            _result = _calculator.Multiply();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
---
+++ .NET 6/ClassLibrary/SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Subtract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+
+        public int Divide()
+        {
+            if (FirstNumber == 0 || SecondNumber == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return FirstNumber / SecondNumber;
+            }
+        }
+
+        public int Multiply()
+        {
+            return FirstNumber * SecondNumber;
+        }
+    }
+}
