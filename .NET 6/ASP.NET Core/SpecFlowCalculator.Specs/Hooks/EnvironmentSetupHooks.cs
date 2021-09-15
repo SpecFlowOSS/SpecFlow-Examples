2ed0c1d2252933138d68bc97c9b0b2e97c369b32
Jordan Western <j.western@tricentis.com>
2021-09-15 17:05:56 +0100

    path to process

--- .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
+++ .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
@@ -6,18 +6,19 @@ namespace SpecFlowCalculator.Specs.Hooks
     [Binding]
     public sealed class EnvironmentSetupHooks
     {
-        private static Process _webApp;
-
         [BeforeTestRun]
         public static void BeforeScenario()
         {
-            _webApp = Process.Start(@"C:\Users\jorwe\source\repos\SpecFlow-Examples\.NET 6\ASP.NET Core\SpecFlowCalculator\bin\Debug\net6.0\SpecFlowCalculator.exe");
+            Process.Start(@"../../../../SpecFlowCalculator/bin/Debug/net6.0/SpecFlowCalculator.exe");
         }
 
         [AfterTestRun]
         public static void AfterScenario()
         {
-            _webApp.Dispose();
+            foreach (var runningProcess in Process.GetProcessesByName("SpecFlowCalculator.exe"))
+            {
+                runningProcess.Dispose();
+            }
         }
     }
 }
