5bd205987d4b80583f05541b3f8657d42ac11245
Jordan Western <j.western@tricentis.com>
2021-09-15 10:41:31 +0100

    resolved project running issues

--- .NET 6/ASP.NET Core/SpecFlowCalculator/Startup.cs
+++ .NET 6/ASP.NET Core/SpecFlowCalculator/Startup.cs
@@ -23,7 +23,11 @@ namespace SpecFlowCalculator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            // services.AddControllersWithViews();
+
+            // Required for .NET 6 - Install package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
