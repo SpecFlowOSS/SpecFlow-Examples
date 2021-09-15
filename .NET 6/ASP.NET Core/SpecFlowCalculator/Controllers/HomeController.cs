0d978d81fd3c95cc04837a14c8188a16305d4e2b
Jordan Western <j.western@tricentis.com>
2021-09-15 12:40:37 +0100

    completed web app sample for ASP.NET core Calculator

--- .NET 6/ASP.NET Core/SpecFlowCalculator/Controllers/HomeController.cs
+++ .NET 6/ASP.NET Core/SpecFlowCalculator/Controllers/HomeController.cs
@@ -8,20 +8,49 @@ namespace SpecFlowCalculator.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Calculator _calculator;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _calculator = new Calculator();
         }
 
-        public IActionResult Index()
+        public IActionResult Index(CalculatorViewModel calculatorViewModel)
         {
-            return View();
+            return View(calculatorViewModel);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Add(CalculatorViewModel calculatorViewModel)
         {
-            return View();
+            calculatorViewModel.Result =
+                _calculator.Add(calculatorViewModel.FirstNumber, calculatorViewModel.SecondNumber);
+
+            return View("Index", calculatorViewModel);
+        }
+
+        public IActionResult Subtract(CalculatorViewModel calculatorViewModel)
+        {
+            calculatorViewModel.Result =
+                _calculator.Subtract(calculatorViewModel.FirstNumber, calculatorViewModel.SecondNumber);
+
+            return View("Index", calculatorViewModel);
+        }
+
+        public IActionResult Multiply(CalculatorViewModel calculatorViewModel)
+        {
+            calculatorViewModel.Result =
+                _calculator.Multiply(calculatorViewModel.FirstNumber, calculatorViewModel.SecondNumber);
+
+            return View("Index", calculatorViewModel);
+        }
+
+        public IActionResult Divide(CalculatorViewModel calculatorViewModel)
+        {
+            calculatorViewModel.Result =
+                _calculator.Divide(calculatorViewModel.FirstNumber, calculatorViewModel.SecondNumber);
+
+            return View("Index", calculatorViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
