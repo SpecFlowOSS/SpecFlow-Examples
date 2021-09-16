310aa59055d94e4450d7de292cac84b258ca24bc
Jordan Western <j.western@tricentis.com>
2021-09-16 14:46:55 +0100

    changed to the corrct file path...

--- .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
+++ .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
@@ -14,7 +14,7 @@ namespace SpecFlowCalculator.Specs.Hooks
         {
             // _process = Process.Start("../../../../SpecFlowCalculator/bin/Debug/net6.0/SpecFlowCalculator.exe");
 
-            _process = Process.Start(@"D:\a\1\s\.NET 6\ASP.NET Core\SpecFlowCalculator.Specs\obj\Debug\net6.0\SpecFlowCalculator.exe");
+            _process = Process.Start(@"D:\a\1\s\.NET 6\ASP.NET Core\SpecFlowCalculator\obj\Debug\net6.0\SpecFlowCalculator.exe");
         }
 
         [AfterTestRun]
