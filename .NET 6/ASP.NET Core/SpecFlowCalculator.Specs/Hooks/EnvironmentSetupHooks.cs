116c5a83b4c29ed94c078007072b8f8c1287af0a
Jordan Western <j.western@tricentis.com>
2021-09-17 08:17:25 +0100

    update hook names

--- .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
+++ .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
@@ -10,17 +10,17 @@ namespace SpecFlowCalculator.Specs.Hooks
         private static Process _process;
 
         [BeforeTestRun]
-        public static void BeforeScenario()
+        public static void BeforeTestRun()
         {
-            // _process = Process.Start("../../../../SpecFlowCalculator/bin/Debug/net6.0/SpecFlowCalculator.exe");
+            _process = Process.Start("../../../../SpecFlowCalculator/bin/Debug/net6.0/SpecFlowCalculator.exe");
 
-            Directory.SetCurrentDirectory(@"D:\a\1\s\.NET 6\ASP.NET Core\SpecFlowCalculator\obj\Debug\net6.0");
+            // Directory.SetCurrentDirectory(@"D:\a\1\s\.NET 6\ASP.NET Core\SpecFlowCalculator\obj\Debug\net6.0\SpecFlowCalculator.exe");
 
-            _process = Process.Start(@"SpecFlowCalculator.exe");
+            // _process = Process.Start(@"D:\a\1\s\.NET 6\ASP.NET Core\SpecFlowCalculator\obj\Debug\net6.0\SpecFlowCalculator.exe");
         }
 
         [AfterTestRun]
-        public static void AfterScenario()
+        public static void AfterTestRun()
         {
             // Close() or Dispose() do not stop the process
             _process.Kill();
