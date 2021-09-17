a15744f172cd8864e8cddac23d8265fd55b9973d
Jordan Western <j.western@tricentis.com>
2021-09-17 10:14:53 +0100

    adding some debugging

--- .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
+++ .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/EnvironmentSetupHooks.cs
@@ -1,5 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Threading;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Events;
 
 namespace SpecFlowCalculator.Specs.Hooks
 {
@@ -8,10 +12,21 @@ namespace SpecFlowCalculator.Specs.Hooks
     {
         private static Process _process;
 
+        private static bool started = false;
+
+        private static int retries = 0;
+
         [BeforeTestRun]
         public static void BeforeTestRun()
         {
+            Console.WriteLine("Starting SpecFlowCalculator.exe...");
+
             _process = Process.Start("../../../../SpecFlowCalculator/bin/Debug/net6.0/SpecFlowCalculator.exe");
+
+            Console.WriteLine("Polling SpecFlowCalculator.exe...");
+
+            // Debug
+            PollProcess();
         }
 
         [AfterTestRun]
@@ -19,6 +34,34 @@ namespace SpecFlowCalculator.Specs.Hooks
         {
             // Close() or Dispose() do not stop the process
             _process.Kill();
+        }
+
+        // Debug
+        private static void PollProcess()
+        {
+            while (!started)
+            {
+                if (retries < 5)
+                {
+                    try
+                    {
+                        Process.GetProcessById(_process.Id);
+                    }
+                    catch (Exception e)
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(5));
+                        Console.WriteLine("Waiting for 5 seconds for process SpecFlowCalculator.exe...");
+                        retries++;
+                    } 
+                }
+                else
+                {
+                    throw new Exception("Process SpecFlowCalculator.exe failed to start");
+                }
+
+                started = true;
+                Console.WriteLine("SpecFlowCalculator.exe started");
+            }
         }
     }
 }
--- .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/TestExecutionHooks.cs
+++ .NET 6/ASP.NET Core/SpecFlowCalculator.Specs/Hooks/TestExecutionHooks.cs
@@ -1,5 +1,8 @@
-﻿using SpecFlowCalculator.Specs.PageObjects;
+﻿using System;
+using SpecFlow.Actions.Selenium;
+using SpecFlowCalculator.Specs.PageObjects;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.CommonModels;
 
 namespace SpecFlowCalculator.Specs.Hooks
 {
@@ -7,9 +10,15 @@ namespace SpecFlowCalculator.Specs.Hooks
     public sealed class TestExecutionHooks
     {
         [BeforeScenario]
-        public void BeforeScenario(CalculatorPage calculatorPage)
+        public void BeforeScenario(CalculatorPage calculatorPage, IBrowserInteractions browserInteractions)
         {
             calculatorPage.Goto();
+
+            // Debug
+
+            Console.WriteLine("Waiting for driver to have url https://localhost:5001/");
+
+            browserInteractions.WaitUntil(browserInteractions.GetUrl, result => result.Equals("https://localhost:5001/"));
         }
     }
 }
