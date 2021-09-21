94aa390ec15fb726ea2409b26ad19f979baefbaa
Jordan Western <j.western@tricentis.com>
2021-09-21 16:36:56 +0100

    implementation and example complete

---
+++ .NET 6/ASP.NET Core API/SpecFlowCalculator.Specs/API/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+
+namespace SpecFlowCalculator.Specs.API
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+
+        public int SecondNumber { get; set; }
+
+        private readonly RestClient _client;
+
+        public Calculator()
+        {
+            _client = new RestClient("https://localhost:5001");
+        }
+
+        public async Task<int> AddAsync()
+        {
+            var request = new RestRequest("Calculator/Add").AddObject(this);
+
+            var response =  await _client.GetAsync<CalculatorResponse>(request);
+
+            return response.Result;
+        }
+
+        public async Task<int> SubtractAsync()
+        {
+            var request = new RestRequest("Calculator/Subtract").AddObject(this);
+
+            var response = await _client.GetAsync<CalculatorResponse>(request);
+
+            return response.Result;
+        }
+
+        public async Task<int> MultiplyAsync()
+        {
+            var request = new RestRequest("Calculator/Multiply").AddObject(this);
+
+            var response = await _client.GetAsync<CalculatorResponse>(request);
+
+            return response.Result;
+        }
+
+        public async Task<int> DivideAsync()
+        {
+            var request = new RestRequest("Calculator/Divide").AddObject(this);
+
+            var response = await _client.GetAsync<CalculatorResponse>(request);
+
+            return response.Result;
+        }
+    }
+
+    internal sealed class CalculatorResponse
+    {
+        public int Result { get; set; }
+    }
+}
