c49e9dddf4e7f4baecc859c27f1d51c6af62ecd6
Jordan Western <j.western@tricentis.com>
2021-09-24 11:34:47 +0100

    winform .NET 6 example complete

---
+++ .NET 6/WinForms/SpecFlowCalculator.Specs/Hooks/Hooks.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowCalculator.Specs.Hooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        private static Process _process;
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            _process = Process.Start(@"C:\Program Files\Windows Application Driver\WinAppDriver.exe");
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            _process.Kill();
+        }
+    }
+}
---
+++ .NET 6/WinForms/SpecFlowCalculator/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SpecFlowCalculator
+{
+    public partial class Form1 : Form
+    {
+        private readonly Calculator _calculator;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _calculator = new Calculator();
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            _calculator.FirstNumber = int.Parse(textBox_firstNumber.Text);
+            _calculator.SecondNumber = int.Parse(textBox_secondNumber.Text);
+            _calculator.Add();
+            textBox_result.Text = _calculator.Result.ToString();
+        }
+
+        private void button_subtract_Click(object sender, EventArgs e)
+        {
+            _calculator.FirstNumber = int.Parse(textBox_firstNumber.Text);
+            _calculator.SecondNumber = int.Parse(textBox_secondNumber.Text);
+            _calculator.Subtract();
+            textBox_result.Text = _calculator.Result.ToString();
+        }
+
+        private void button_multiply_Click(object sender, EventArgs e)
+        {
+            _calculator.FirstNumber = int.Parse(textBox_firstNumber.Text);
+            _calculator.SecondNumber = int.Parse(textBox_secondNumber.Text);
+            _calculator.Multiply();
+            textBox_result.Text = _calculator.Result.ToString();
+        }
+
+        private void button_divide_Click(object sender, EventArgs e)
+        {
+            _calculator.FirstNumber = int.Parse(textBox_firstNumber.Text);
+            _calculator.SecondNumber = int.Parse(textBox_secondNumber.Text);
+            _calculator.Divide();
+            textBox_result.Text = _calculator.Result.ToString();
+        }
+    }
+}
