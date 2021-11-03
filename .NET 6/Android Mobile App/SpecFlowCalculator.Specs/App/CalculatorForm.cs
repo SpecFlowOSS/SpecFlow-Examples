bf9cc23bdeaac9ee4f72fa60c13fd60be3a0a524
Jordan Western <j.western@tricentis.com>
2021-11-03 11:50:52 +0000

    updates to locators

--- .NET 6/Android Mobile App/SpecFlowCalculator.Specs/App/CalculatorForm.cs
+++ .NET 6/Android Mobile App/SpecFlowCalculator.Specs/App/CalculatorForm.cs
@@ -13,24 +13,24 @@ namespace SpecFlowCalculator.Specs.App
         }
 
         public AndroidElement FirstNumberTextBox =>
-            _appDriver.Current.FindElementById("android:id/firstNumberTextBox'");
+            _appDriver.Current.FindElementById("com.companyname.specflowcalculator:id/firstNumberTextBox");
 
         public AndroidElement SecondNumberTextBox =>
-            _appDriver.Current.FindElementById("android:id/secondNumberTextBox'");
+            _appDriver.Current.FindElementById("com.companyname.specflowcalculator:id/secondNumberTextBox");
 
         public AndroidElement AddButton =>
-            _appDriver.Current.FindElementById("android:id/addButton");
+            _appDriver.Current.FindElementById("com.companyname.specflowcalculator:id/addButton");
 
         public AndroidElement SubtractButton =>
-            _appDriver.Current.FindElementById("android:id/subtractButton");
+            _appDriver.Current.FindElementById("com.companyname.specflowcalculator:id/subtractButton");
 
         public AndroidElement MultiplyButton =>
-            _appDriver.Current.FindElementById("android:id/multiplyButton");
+            _appDriver.Current.FindElementById("com.companyname.specflowcalculator:id/multiplyButton");
 
         public AndroidElement DivideButton =>
-            _appDriver.Current.FindElementById("android:id/divideButton");
+            _appDriver.Current.FindElementById("com.companyname.specflowcalculator:id/divideButton");
 
         public AndroidElement ResultTextBox =>
-            _appDriver.Current.FindElementById("android:id/resultTextBox");
+            _appDriver.Current.FindElementById("com.companyname.specflowcalculator:id/resultTextBox");
     }
 }
