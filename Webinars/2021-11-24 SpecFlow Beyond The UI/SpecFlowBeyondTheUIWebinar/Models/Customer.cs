64240385c6d5f8de609cfea7924d0e712140a4df
Jordan Western <49209919+JordanWestern@users.noreply.github.com>
2021-11-24 08:51:38 +0000

    Sync with Bas' repo (#89)
    * pushes project example for 'Beyong the UI webinar'
    
    * removes git folder
    
    * syncs with repo from Bas
    
    Co-authored-by: Jordan Western <j.western@tricentis.com>

--- Webinars/2021-11-24 SpecFlow Beyond The UI/SpecFlowBeyondTheUIWebinar/Models/Customer.cs
+++ Webinars/2021-11-24 SpecFlow Beyond The UI/SpecFlowBeyondTheUIWebinar/Models/Customer.cs
@@ -5,7 +5,9 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public int Id { get; set; }
-        public List<Account> Accounts { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public List<Account> Accounts { get; set; } = default!;
 
         public void CreateNewAccount(Account newAccount)
         {
