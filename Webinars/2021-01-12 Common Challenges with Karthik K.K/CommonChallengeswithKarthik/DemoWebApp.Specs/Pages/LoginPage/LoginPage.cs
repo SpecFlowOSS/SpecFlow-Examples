8dd79abbc0091615b0b6c0f504bed68e625d929e
Jordan Western <j.western@tricentis.com>
2022-01-04 20:05:22 +0000

    includes waiting strategy

--- Webinars/2021-01-12 Common Challenges with Karthik K.K/CommonChallengeswithKarthik/DemoWebApp.Specs/Pages/LoginPage/LoginPage.cs
+++ Webinars/2021-01-12 Common Challenges with Karthik K.K/CommonChallengeswithKarthik/DemoWebApp.Specs/Pages/LoginPage/LoginPage.cs
@@ -1,4 +1,6 @@
 ﻿using DemoWebApp.Specs.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SpecFlow.Actions.Selenium;
 
 namespace DemoWebApp.Specs.Pages.LoginPage
@@ -25,7 +27,38 @@ namespace DemoWebApp.Specs.Pages.LoginPage
         {
             UsernameField.SendKeys(username);
             PasswordField.SendKeys(password);
-            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            // ---- Hard wait ------
+            // Useful if you cannot leverage the other waits, i.e. there is no DOM state change or randomness in async execution delays
+
+            // Thread.Sleep(TimeSpan.FromSeconds(2));
+            // ---------------------
+
+            // ---- Implicit wait ----
+            // happens by setting the driver timeouts during initialisation (in this case via SpecFlow.Actions.Selenium via specflow.actions.json config file)
+            // It is by default applied to all the elements in the script.
+            // We cannot wait based on a specified condition like element selectable / clickable unlike explicit.
+            // -----------------------
+
+            //---- Explicit wait ----
+            // It is applicable to only a certain element which is specific to a certain condition.
+            // It is usually used, when you are not aware of the time of the element visibility. It is subjected to dynamic nature.
+            // Can ignore exceptions
+
+            var waitDriver = new WebDriverWait(
+                new SystemClock(),
+                _browserDriver.Current,
+                TimeSpan.FromSeconds(30),
+                TimeSpan.FromSeconds(1));
+
+            waitDriver.IgnoreExceptionTypes(
+                typeof(ElementClickInterceptedException),
+                typeof(ElementNotInteractableException),
+                typeof(ElementNotSelectableException));
+
+            waitDriver.Until(x => SubmitButton.Enabled);
+            // ---------------------
+
             SubmitButton.Click();
         }
     }
