454032910ec00ff06894d9643c8f7e589153a3e3
Andreas Willich <a.willich@tricentis.com>
2022-03-31 10:14:57 +0200

    add docker example

---
+++ Actions/DockerExample/DockerExample/Check Webserver.feature
@@ -0,0 +1,10 @@
+﻿Feature: First Startup
+
+
+Scenario: Installation Page is shown
+
+	Given a new installation of WordPress
+
+	When the user first visits
+
+	Then the user is presented the installation start page
---
+++ Actions/DockerExample/DockerExample/Steps.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using SpecFlow.Actions.Selenium;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.Actions.Docker.IntegrationTests
+{
+    [Binding]
+    public class Steps
+    {
+        private readonly IBrowserInteractions _browserInteractions;
+        private readonly BrowserDriver _browserDriver;
+
+        public Steps(IBrowserInteractions browserInteractions, BrowserDriver browserDriver)
+        {
+            _browserInteractions = browserInteractions;
+            _browserDriver = browserDriver;
+        }
+
+        [Given("a new installation of WordPress")]
+        public void ANewInstallationOfWordPress()
+        {
+
+        }
+
+        [When("the user first visits")]
+        public void TheUserFirstVisits()
+        {
+            _browserInteractions.GoToUrl("http://localhost:8000");
+        }
+
+        [Then("the user is presented the installation start page")]
+        public void TheUserIsPresentedTheInstallationStartPage()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                var currentTitle = _browserDriver.Current.Title;
+
+                if (currentTitle != "WordPress › Installation")
+                {
+                    _browserDriver.Current.Navigate().Refresh();
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+            }
+
+            _browserDriver.Current.Title.Should().Be("WordPress › Installation");
+        }
+    }
+}
