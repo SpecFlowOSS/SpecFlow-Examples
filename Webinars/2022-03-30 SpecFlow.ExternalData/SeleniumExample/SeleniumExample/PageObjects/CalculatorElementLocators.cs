bfe2a0d662a50e05865ee56458aab8a12f4374a1
Andreas Willich <SabotageAndi@users.noreply.github.com>
2022-05-18 16:11:42 +0200

    add SpecFlow.ExternalData Webinar example (#104)

---
+++ Webinars/2022-03-30 SpecFlow.ExternalData/SeleniumExample/SeleniumExample/PageObjects/CalculatorElementLocators.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+
+namespace Example.PageObjects
+{
+    public class CalculatorElementLocators
+    {
+        //The URL of the calculator to be opened in the browser
+        private protected const string CalculatorUrl = "https://specflowoss.github.io/Calculator-Demo/Calculator.html";
+
+        //Finding elements by ID
+        private protected By FirstNumberFieldLocator => By.Id("first-number");
+        private protected By SecondNumberFieldLocator => By.Id("second-number");
+        private protected By AddButtonLocator => By.Id("add-button");
+        private protected By ResultLabelLocator => By.Id("result");
+        private protected By ResetButtonLocator => By.Id("reset-button");
+    }
+}
