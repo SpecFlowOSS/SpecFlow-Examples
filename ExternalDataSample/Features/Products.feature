8c488c75ce574b69457fe673c0946a2eb0ef35b6
Andreas Willich <a.willich@tricentis.com>
2022-06-22 14:48:21 +0200

    add excel sample

---
+++ ExternalDataSample/Features/Products.feature
@@ -0,0 +1,9 @@
+﻿Feature: Products
+
+
+@DataSource:products.xlsx @DataSet:other_products
+Scenario: The basket price is calculated correctly for other products
+	Given the price of <product> is €<price>
+	And the customer has put 1 piece of <product> in the basket
+	When the basket price is calculated
+	Then the basket price should be €<price>
---
+++ ExternalDataSample/Steps/ProductBindings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Specs.Steps
+{
+    [Binding]
+    public class ProductBindings
+    {
+        [Given(@"the price of (.*) is €(.*)")]
+        public void GivenThePriceOfProductIsPrice(string product, float price)
+        {
+        }
+
+        [Given(@"the customer has put (.*) piece of (.*) in the basket")]
+        public void GivenTheCustomerHasPutPieceOfProductInTheBasket(int number, string product)
+        {
+        }
+
+        [When(@"the basket price is calculated")]
+        public void WhenTheBasketPriceIsCalculated()
+        {
+        }
+
+        [Then(@"the basket price should be €(.*)")]
+        public void ThenTheBasketPriceShouldBePrice(string price)
+        {
+        }
+
+    }
+}
